61dd81b906f05507c5fa07e481e932d2fb93c062
Lunar Starstrum <lunar@forsaken-borders.net>
2023-12-27 17:48:28 -0600

    Remove unused async events

--- src/EventArgs/DeepgramEventArgs.cs
+++
@@ -1,9 +0,0 @@
-using DeepgramSharp.AsyncEvents;
-
-namespace DeepgramSharp.EventArgs
-{
-    /// <summary>
-    /// Represents the base class for Deepgram asynchronous event arguments.
-    /// </summary>
-    public abstract class DeepgramEventArgs : AsyncEventArgs;
-}
--- src/EventArgs/DeepgramLivestreamClosedEventArgs.cs
+++
@@ -1,7 +0,0 @@
-namespace DeepgramSharp.EventArgs
-{
-    /// <summary>
-    /// Represents the event arguments for when a Deepgram livestream is closed.
-    /// </summary>
-    public sealed class DeepgramLivestreamClosedEventArgs : DeepgramEventArgs;
-}
